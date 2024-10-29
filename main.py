f68431727ac7c87a75cf52ddbfac701377369933
princebeersingh <princebeersingh@gmail.com>
2024-10-29 23:23:52 +0530

    shema introduced

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 
 app =FastAPI()
 
+
+class Post(BaseModel):
+      title:str
+      content:str
+      published: bool = True
+      rating: Optional[int] = None
+ 
 @app.get("/")
 async def root():
       return {"message": "to app"}
@@ -13,6 +22,8 @@ def get_posts():
       return {"data": "this is your post"}
 
 @app.post("/createposts")
-def create_posts(payLoad: dict = Body(...)):
-      
-      return {"new post":f"title : {payLoad['title']}, content : {payLoad['content']}"}
+def create_posts(post:Post):#here data converted into python dictionary
+      print(post)
+      print(post.dict())
+      return {"data":post}
+# title -string, content -string , category, bool published
